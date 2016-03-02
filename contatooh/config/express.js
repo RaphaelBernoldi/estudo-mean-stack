8f68bb3b1116b8c2249e9e9092eb8179077c435a
Raphael Bernoldi <raphaelbernoldi@gmil.com>
2016-03-02 20:48:23 -0300

    Confirguracao do express e pagina publica para ver app funcionando

--- contatooh/config/express.js
+++ contatooh/config/express.js
@@ -3,6 +3,13 @@ var express = require('express');
 module.exports = function(){
 	var app = express();
 	
+	//Configuração de porta
+	app.set('port', 3000);
+	
+	//Torando pasta public publica e acessivel pelo browser
+	//obs o ./ sinaliza a pasta onde o script foi executado
+	app.use(express.static('./public'));
+	
 	return app;
 
 }
