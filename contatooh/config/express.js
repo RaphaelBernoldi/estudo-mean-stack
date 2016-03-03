7ed30fd82ffc5027f2cda8e855a405c68171e000
Raphael Bernoldi <raphaelbernoldi@gmil.com>
2016-03-02 23:15:26 -0300

    Inclusao da configuracao dos dois novos modulos(body-parser/method-override) no arquivo de configuracao do express e atualizacao das rotas para carregarem os controllers pela instancia do express

--- contatooh/config/express.js
+++ contatooh/config/express.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
 module.exports = function(){
 	var app = express();
@@ -14,6 +15,10 @@ module.exports = function(){
 	//configuracao do view engine (especie de jsp) processada do lado do servidor
 	app.set('view engine', 'ejs');
 	app.set('views','./app/views');
+	
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.json());
+	app.use(require('method-override')());
 	
 	//Já instancia objetos e injeta na instancia do express assim evitando ficar usando um monte de require
 	load('models',{cwd:'app'})
