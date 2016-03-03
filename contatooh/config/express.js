ee2a9cc8938fb6fbd032ff70b9313daabfa62400
Raphael Bernoldi <raphaelbernoldi@gmil.com>
2016-03-02 21:26:46 -0300

    Inclusao do modulo ejs (que processa view do lado do servidor), criacao de rota e de controller backend

--- contatooh/config/express.js
+++ contatooh/config/express.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var home = require('../app/routes/home')
 
 module.exports = function(){
 	var app = express();
@@ -9,6 +10,14 @@ module.exports = function(){
 	//Torando pasta public publica e acessivel pelo browser
 	//obs o ./ sinaliza a pasta onde o script foi executado
 	app.use(express.static('./public'));
+	
+	//Instancia de objeto home com as rotas de home
+	home(app);
+	
+	//configuracao do view engine (especie de jsp) processada do lado do servidor
+	app.set('view engine', 'ejs');
+	app.set('views','./app/views');
+	
 	
 	return app;
 
